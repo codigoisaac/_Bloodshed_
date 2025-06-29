b5cea2958f7d2efde29513ae466841c9a1af1b24
Isaac Muniz <im.isaac.muniz@gmail.com>
2025-06-29 15:45:27 -0300

    refactor: Add Django views and URL routing for home and room pages

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,4 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-
-def home(request):
-    return HttpResponse("Hello, Django! This is the Home Page.")
-
-
-def room(request):
-    return HttpResponse("This is the secret room.")
-
-
-urlpatterns = [path("admin/", admin.site.urls), path("", home), path("room/", room)]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("base.urls"))]
