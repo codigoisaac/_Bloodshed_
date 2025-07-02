d80b6d81a3901308e4dcdbe45d68aaa13d1086dd
Isaac Muniz <im.isaac.muniz@gmail.com>
2025-07-02 18:25:29 -0300

    refactor: Update views and templates for home and room pages

--- base/views.py
+++ base/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
+rooms = [
+    {"id": 1, "name": "Let's learn Python!"},
+    {"id": 2, "name": "Let's learn Angular!"},
+    {"id": 3, "name": "Let's learn Goang!"},
+    {"id": 4, "name": "Let's learn C#!"},
+]
+
 
 def home(request):
-    return render(request, "home.html")
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
 
 
 def room(request):
-    return render(request, "room.html")
+    return render(request, "base/room.html")
