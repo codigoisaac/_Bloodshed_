c8908029ac0a18c55fc29fb2bfb9f57dd071673d
Isaac Muniz <im.isaac.muniz@gmail.com>
2025-07-03 12:00:54 -0300

    feat: Implement Room model and register it in admin

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    # host =
+    # topic =
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants =
+    updatedAt = models.DateTimeField(auto_now=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
